13415771398f70c8e6941465c69253d0a7213b8b
root <root@winra.winllc-dev.com>
2021-05-28 15:37:48 -0400

    began adding prometheus

--- data/keycloak/scipts/init-config.sh
+++ data/keycloak/scipts/init-config.sh
@@ -14,4 +14,8 @@ $KDADMDIRECTORY/kcadm.sh set-password -r dev --username win-ra-admin --new-passw
 
 $KDADMDIRECTORY/kcadm.sh add-roles -r dev --uusername win-ra-admin --cclientid realm-management --rolename realm-admin
 
+$KDADMDIRECTORY/kcadm.sh add-roles -r dev --uusername winra_admin --cclientid pki-ra-client-public --rolename SUPER_ADMIN
+$KDADMDIRECTORY/kcadm.sh add-roles -r dev --uusername winra_admin --cclientid realm-management --rolename realm-admin
+
+
 #$KDADMDIRECTORY/kcadm.sh create clients -r dev -s clientId=pki-ra-client-public -s enabled=true -s publicClient=true -s secret=9bbacdcc-6bbc-4c23-8a7a-77864a23a586
