e7c699979c9127d294da499fcf323d9615a328e7
root <root@winra.winllc-dev.com>
2021-06-02 17:27:48 -0400

    better prometheus

--- start-ra.sh
+++ start-ra.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 if [ -x "$(command -v docker)" ]; then
+    FILES="./images/*"
+    for f in $FILES
+    do
+      echo "Importing Docker Image $f ..."
+      # take action on each file. $f store current file name
+      docker import $f
+    done
+
     docker-compose -f docker-compose.yaml up -d
 
     docker exec win-ra-keycloak /scripts/init-config.sh
