5a2f869a550c0dee0010ef0333f474236abfc75d
David Winters <96264505+winllc@users.noreply.github.com>
2021-12-30 17:35:15 -0500

    Update build_projects.sh

--- build_projects.sh
+++ build_projects.sh
@@ -16,7 +16,7 @@ echo "Building pki-ra-api-server"
 cd $TOPDIR/pki-ra-api-server
 ./gradlew -x test build
 
-echo "Building pki-ra-est-server"
-cd $TOPDIR/pki-ra-est-server
-./mvnw install:install-file    -Dfile=$(pwd)/../pki-ra-common/build/libs/pki-ra-common-1.0-SNAPSHOT.jar    -DgroupId=com.winllc.acme.common    -DartifactId=pki-ra-common    -Dversion=1.0-SNAPSHOT    -Dpackaging=jar    -DgeneratePom=true
-./mvnw clean install
+#echo "Building pki-ra-est-server"
+#cd $TOPDIR/pki-ra-est-server
+#./mvnw install:install-file    -Dfile=$(pwd)/../pki-ra-common/build/libs/pki-ra-common-1.0-SNAPSHOT.jar    -DgroupId=com.winllc.acme.common    -DartifactId=pki-ra-common    -Dversion=1.0-SNAPSHOT    -Dpackaging=jar    -DgeneratePom=true
+#./mvnw clean install
